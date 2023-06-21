9a3e30a1385a24753c5998dfb49488cc3cd531df
Kylmegawar <kylmegawar@gmail.com>
2023-06-21 15:40:37 +0200

    Propre

--- test/Project-MFB-opacit-/gfxlib/src/ErreurLib.c
+++
@@ -1,5 +0,0 @@
-#include "../include/ErreurLib.h"
-
-const char *ChaineErreurLisEntier = "La valeur entree n'est pas de type entier. Veuillez recommencer SVP : ";
-const char *ChaineErreurLisFlottant = "La valeur entree n'est pas de type flottant. Veuillez recommencer SVP : ";
-const char *ChaineErreurLisCaractere = "La valeur entree n'est pas de type caractere. Veuillez recommencer SVP : ";
